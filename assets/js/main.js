78efc4a5390e1bad4f9de73e9fd620a5d9eb0334
puja saund <146694494+assistantsiri-255@users.noreply.github.com>
2023-11-02 18:25:07 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,44 +1,27 @@
-//alert("hello from js");
-
 function convert(){
-	//	alert("hello it's convert");
-	//create object or refenc
-	
-		var frm,to,amount;
-		
-		frm=document.getElementById('from');
-		to=document.getElementById('to');
-		amount=document.getElementById('amt');
-		res=document.getElementById('result');
-		cu=document.getElementById('curr');
-		
-	/*document.write(frm+"<br>");
-	document.write(to+"<br>");
-	document.write(amount+"<br>");
-	
-	document.write("from :"+frm.value+"<br>");
-	document.write("to :"+to.value+"<br>");
-	document.write("amount :"+amount.value+"<br>");*/
-	
-	var url='https://v6.exchangerate-api.com/v6/a5a557e19f0c52c5f6e7cbb2/latest/'+frm.value;
-	//document.write(url);
-		fetch(url)
-		.then(function(res){
-			
-	//document.write(Res/json ());
+	// alert("this is convert button");
+
+	var x,y,z;
+	x= document.getElementById('from'); //AMERICA
+	y = document.getElementById('to'); //INDIA
+	z = document.getElementById('amt'); //100 USD 
+	var final = document.getElementById('pooja');
+	var S = 'https://v6.exchangerate-api.com/v6/eab7e00d7c6c00617e242dee/latest/'+x.value;
+	fetch(S).then(function(res){
 		return res.json();
 	}).then(function(data){
-		/*document.write(data+"<br>");
-		document.write(data['result']+"<br>");
-		document.write(data['base_code']+"<br>");*/
-		var cr=data['conversion_rates'];
-		//document.write(data['conversion_rates']+"<br>");
-		cf=cr[to.value];
+		// document.write(data,"<br>");
+		// document.write(data['result'],"<br>");
+		// document.write(data['conversion_rates'],"<br>");
+		var cr = data['conversion_rates'];
+		var cf = cr[to.value]; // 83 
+		var fam = cf * amt.value; // 83*100
 		
-		var famt=cf*amount.value;
-		//document.write("Final AMOUNT:"+famt);
-		res.innerHTML=famt;
-		cu.innerHTML=to.value;
+		final.innerHTML = fam;
 		
-	});
+	
+	})
 }
+// x = 3;
+// if x = 3 ;
+//print ("hello world");
